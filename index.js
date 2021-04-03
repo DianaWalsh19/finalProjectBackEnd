e0150d49af3c1ef29139dd96d08b950e0209b386
Diana Walsh <diwalsh@hubspot.local>
2021-04-03 17:55:58 +0100

    First approach to API

---
+++ index.js
@@ -0,0 +1,85 @@
+const Joi = require("joi");
+//const validator = require("express-joi-validation").createValidator({});
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const users = [
+  {
+    id: 1,
+    email: "patient1@patients.com",
+    password: "passwordUser1",
+  },
+
+  {
+    id: 2,
+    email: "patient2@patients.com",
+    password: "passwordUser2",
+  },
+
+  {
+    id: 3,
+    email: "patient3@patients.com",
+    password: "passwordUser3",
+  },
+];
+
+app.get("/api/users", (req, res) => {
+  res.send(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user)
+    return res.status(404).send("The user with the given ID was not found.");
+  res.send(user);
+});
+
+app.post("/api/users", (req, res) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = {
+    id: users.length + 1,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  users.push(user);
+  res.send(user);
+});
+
+app.put("/api/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user)
+    return res.status(404).send("The user with the given ID was not found.");
+
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  user.email = req.body.email;
+  res.send(user);
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user)
+    return res.status(404).send("The user with the given ID was not found.");
+
+  const index = users.indexOf(user);
+  users.splice(index, 1);
+
+  res.send(user);
+});
+
+function validateUser(user) {
+  const schema = {
+    email: Joi.string(),
+    password: Joi.string().min(8),
+  };
+
+  return Joi.validate(user, schema);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
